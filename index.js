f88f50cee88861d05e8293a6d656635afb2e66b6
Jacob Chademwiri <jchademwiri@gmail.com>
2021-12-26 21:18:43 +0200

    change heroku port

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.use(cors());
 app.use(express.json()); //Used to parse JSON bodies
 app.use(express.urlencoded({ extended: true }));
 
-const port = process.env.port || 8080;
+const PORT = process.env.PORT || 5000;
 // coment
 
-app.listen(port, () => console.info(`Listening on port ${port}`));
+app.listen(PORT, () => console.info(`Listening on port ${PORT}`));
 
 // static files
 app.use(express.static('public'));
@@ -44,7 +44,7 @@ app.use('/api/blog/posts', postRoute);
 
 // Connect to DB
 
-// heroku_url = process.env.MONGODB_URI; 
+// heroku_url = process.env.MONGODB_URI;
 
 // mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
 // 	console.log('Connected to Database');
@@ -54,7 +54,7 @@ mongoose
 	.connect(process.env.DB_CONNECTION, {
 		dbName: 'playhouse',
 		useNewUrlParser: true,
-		useUnifiedTopology: true,
+		useUnifiedTopology: true
 	})
 	.then(() => console.log('Connected to Database!'))
 	.catch((err) =>
